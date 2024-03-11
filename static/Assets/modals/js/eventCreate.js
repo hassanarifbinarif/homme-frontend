7e1716f9b2ba7f600728d1f187a4ce4ffa6bfdc0
hassanarif2811@gmail.com <hassanarif2811@gmail.com>
2024-03-11 13:16:46 +0500

    Made rsvp link optional, resize event image on client side

--- static/Assets/modals/js/eventCreate.js
+++ static/Assets/modals/js/eventCreate.js
@@ -1,3 +1,5 @@
+let resizedDataUrl;
+
 function openCreateEventModal(modalID) {
     let modal = document.querySelector(`#${modalID}`);
     let form = modal.querySelector('form');
@@ -16,50 +18,122 @@ function openCreateEventModal(modalID) {
         document.querySelector('.error-div').classList.add('hide');
         document.querySelector('.create-error-msg').classList.remove('active');
         document.querySelector('.create-error-msg').innerText = "";
+        resizedDataUrl = undefined;
     })
     document.querySelector(`.${modalID}`).click();
 }
 
 
-function verifyEventImage(input) {
-    if (input.files.length > 0) {
-        let label = input.closest('label');
-        let width = 330;
-        let height = 330;
-        const img = document.createElement('img');
-        const selectedImage = input.files[0];
-        const objectURL = URL.createObjectURL(selectedImage);
-        let imageTag = label.querySelector('.event-img');
-        img.onload = function handleLoad() {
-            // console.log(`Width: ${img.width}, Height: ${img.height}`);
-    
-            if (img.width == width && img.height == height) {
-                imageTag.src = objectURL;
-                imageTag.classList.remove('hide');
-                label.querySelector('svg').style.display = 'none';
-                label.querySelectorAll('span').forEach((span) => {
-                    span.style.display = 'none';
-                })
-                document.querySelector('.error-div').classList.add('hide');
-                document.querySelector('.create-error-msg').classList.remove('active');
-                document.querySelector('.create-error-msg').innerText = "";
-            }
-            else {
-                URL.revokeObjectURL(objectURL);
-                imageTag.classList.add('hide');
-                label.querySelector('svg').style.display = 'block';
-                label.querySelectorAll('span').forEach((span) => {
-                    span.style.display = 'block';
-                })
-                document.querySelector('.error-div').classList.remove('hide');
-                document.querySelector('.create-error-msg').classList.add('active');
-                document.querySelector('.create-error-msg').innerText = "Image does not match supported dimensions: 330x330 px";
-                input.value = null;
-            }
+// New method to upload and resize image to 330 x 330
+
+function verifyEventImage(event, input) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    let label = event.target.closest('label');
+    let imageTag = label.querySelector('.event-img');
+
+    reader.onload = function(event) {
+        const img = new Image();
+        img.src = event.target.result;
+
+        img.onload = function() {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            const targetSize = 330;
+            let width = img.width;
+            let height = img.height;
+
+            // If image aspect ratio needs to be retained (introduces black edges horizontally or vertically).
+            // if (width > height) {
+            //     height = Math.round((height * targetSize) / width);
+            //     width = targetSize;
+            // } else {
+            //     width = Math.round((width * targetSize) / height);
+            //     height = targetSize;
+            // }
+
+            // canvas.width = targetSize;
+            // canvas.height = targetSize;
+            // ctx.drawImage(img, (targetSize - width) / 2, (targetSize - height) / 2, width, height);
+
+
+            // No image aspect ratio retention
+            canvas.width = targetSize;
+            canvas.height = targetSize;
+            ctx.drawImage(img, 0, 0, targetSize, targetSize);
+
+            resizedDataUrl = canvas.toDataURL('image/jpeg');
+            imageTag.src = resizedDataUrl;
+
+            imageTag.classList.remove('hide');
+            label.querySelector('svg').style.display = 'none';
+            label.querySelectorAll('span').forEach((span) => {
+                span.style.display = 'none';
+            })
+            document.querySelector('.error-div').classList.add('hide');
+            document.querySelector('.create-error-msg').classList.remove('active');
+            document.querySelector('.create-error-msg').innerText = "";
         };
+    };
+
+    reader.readAsDataURL(file);
+}
+
+
+// Old method to only accept images of dimensions 330 x 330
+
+// function verifyEventImage(event, input) {
+//     if (input.files.length > 0) {
+//         let label = input.closest('label');
+//         let width = 330;
+//         let height = 330;
+//         const img = document.createElement('img');
+//         const selectedImage = input.files[0];
+//         const objectURL = URL.createObjectURL(selectedImage);
+//         let imageTag = label.querySelector('.event-img');
+//         img.onload = function handleLoad() {
+//             // console.log(`Width: ${img.width}, Height: ${img.height}`);
+    
+//             if (img.width == width && img.height == height) {
+//                 imageTag.src = objectURL;
+//                 imageTag.classList.remove('hide');
+//                 label.querySelector('svg').style.display = 'none';
+//                 label.querySelectorAll('span').forEach((span) => {
+//                     span.style.display = 'none';
+//                 })
+//                 document.querySelector('.error-div').classList.add('hide');
+//                 document.querySelector('.create-error-msg').classList.remove('active');
+//                 document.querySelector('.create-error-msg').innerText = "";
+//             }
+//             else {
+//                 URL.revokeObjectURL(objectURL);
+//                 imageTag.classList.add('hide');
+//                 label.querySelector('svg').style.display = 'block';
+//                 label.querySelectorAll('span').forEach((span) => {
+//                     span.style.display = 'block';
+//                 })
+//                 document.querySelector('.error-div').classList.remove('hide');
+//                 document.querySelector('.create-error-msg').classList.add('active');
+//                 document.querySelector('.create-error-msg').innerText = "Image does not match supported dimensions: 330x330 px";
+//                 input.value = null;
+//             }
+//         };
   
-        img.src = objectURL;
+//         img.src = objectURL;
+//     }
+// }
+
+
+function dataURItoBlob(dataURI, type, filename) {
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = type;
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
     }
+    return new Blob([ab], { type: mimeString });
 }
 
 
@@ -104,19 +178,21 @@ async function createEventForm(event) {
         errorMsg.classList.add('active');
         return false;
     }
-    else if (data.rsvp.trim().length == 0) {
+    // else if (imageInput.files.length == 0) {
+    //     errorDiv.classList.remove('hide');
+    //     errorMsg.innerText = 'Image with supported dimensions is required';
+    //     errorMsg.classList.add('active');
+    //     return false;
+    // }
+    else if (resizedDataUrl == undefined) {
         errorDiv.classList.remove('hide');
-        errorMsg.innerText = 'Enter rsvp link';
-        errorMsg.classList.add('active');
-        return false;
-    }
-    else if (imageInput.files.length == 0) {
-        errorDiv.classList.remove('hide');
-        errorMsg.innerText = 'Image with supported dimensions is required';
+        errorMsg.innerText = 'Image is required';
         errorMsg.classList.add('active');
         return false;
     }
     else {
+        formData.delete('image');
+        formData.append('image', dataURItoBlob(resizedDataUrl, 'image/jpeg', 'image.jpg'), 'image.jpg');
         try {
             errorDiv.classList.add('hide');
             errorMsg.innerText = '';
@@ -127,10 +203,10 @@ async function createEventForm(event) {
             };
             beforeLoad(button);
             let response = await requestAPI(`${apiURL}/admin/content/events`, formData, headers, 'POST');
-            // console.log(response);
             response.json().then(function(res) {
                 // console.log(res);
                 if (response.status == 201) {
+                    resizedDataUrl = undefined;
                     getData();
                     afterLoad(button, 'SAVED');
                     form.removeAttribute('onsubmit');
