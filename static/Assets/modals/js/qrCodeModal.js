a9ce63e9db9817dec1e8aea126c8b874febcdf3c
hassanarif2811@gmail.com <hassanarif2811@gmail.com>
2024-03-18 10:59:50 +0500

    Show source name in qr code modal

--- static/Assets/modals/js/qrCodeModal.js
+++ static/Assets/modals/js/qrCodeModal.js
@@ -1,9 +1,11 @@
-function openQRCodeModal(event, modalId, referralCode, qrCodeString) {
+function openQRCodeModal(event, modalId, referralCode, qrCodeString, sourceName='Scan QR Code') {
     event.stopPropagation();
     let modal = document.getElementById(`${modalId}`);
+    modal.querySelector('#modal-qr-code-source-name').innerText = sourceName;
     modal.querySelector('#modal-referral-code').innerText = referralCode;
     modal.querySelector('#modal-qr-code').src = `data:image/png;base64,${qrCodeString}`;
     modal.addEventListener('hidden.bs.modal', event => {
+        modal.querySelector('#modal-qr-code-source-name').innerText = 'Scan QR Code';
         modal.querySelector('#modal-referral-code').innerText = '';
         modal.querySelector('#modal-qr-code').src = '';  
     })
