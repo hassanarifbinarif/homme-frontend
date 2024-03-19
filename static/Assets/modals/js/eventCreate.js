56413bfbf5a0a4642ca743e75ed974ae5f588bee
hassanarif2811@gmail.com <hassanarif2811@gmail.com>
2024-03-19 15:38:09 +0500

    Fix visual glitch in notificataions page, revert back previous implementation of image validation while creating event

--- static/Assets/modals/js/eventCreate.js
+++ static/Assets/modals/js/eventCreate.js
@@ -26,103 +26,103 @@ function openCreateEventModal(modalID) {
 
 // New method to upload and resize image to 330 x 330
 
-function verifyEventImage(event, input) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+// function verifyEventImage(event, input) {
+//     const file = event.target.files[0];
+//     const reader = new FileReader();
 
-    let label = event.target.closest('label');
-    let imageTag = label.querySelector('.event-img');
+//     let label = event.target.closest('label');
+//     let imageTag = label.querySelector('.event-img');
 
-    reader.onload = function(event) {
-        const img = new Image();
-        img.src = event.target.result;
+//     reader.onload = function(event) {
+//         const img = new Image();
+//         img.src = event.target.result;
 
-        img.onload = function() {
-            const canvas = document.createElement('canvas');
-            const ctx = canvas.getContext('2d');
-            const targetSize = 330;
-            let width = img.width;
-            let height = img.height;
+//         img.onload = function() {
+//             const canvas = document.createElement('canvas');
+//             const ctx = canvas.getContext('2d');
+//             const targetSize = 330;
+//             let width = img.width;
+//             let height = img.height;
 
-            // If image aspect ratio needs to be retained (introduces black edges horizontally or vertically).
-            // if (width > height) {
-            //     height = Math.round((height * targetSize) / width);
-            //     width = targetSize;
-            // } else {
-            //     width = Math.round((width * targetSize) / height);
-            //     height = targetSize;
-            // }
+//             // If image aspect ratio needs to be retained (introduces black edges horizontally or vertically).
+//             // if (width > height) {
+//             //     height = Math.round((height * targetSize) / width);
+//             //     width = targetSize;
+//             // } else {
+//             //     width = Math.round((width * targetSize) / height);
+//             //     height = targetSize;
+//             // }
 
-            // canvas.width = targetSize;
-            // canvas.height = targetSize;
-            // ctx.drawImage(img, (targetSize - width) / 2, (targetSize - height) / 2, width, height);
+//             // canvas.width = targetSize;
+//             // canvas.height = targetSize;
+//             // ctx.drawImage(img, (targetSize - width) / 2, (targetSize - height) / 2, width, height);
 
 
-            // No image aspect ratio retention
-            canvas.width = targetSize;
-            canvas.height = targetSize;
-            ctx.drawImage(img, 0, 0, targetSize, targetSize);
+//             // No image aspect ratio retention
+//             canvas.width = targetSize;
+//             canvas.height = targetSize;
+//             ctx.drawImage(img, 0, 0, targetSize, targetSize);
 
-            resizedDataUrl = canvas.toDataURL('image/jpeg');
-            imageTag.src = resizedDataUrl;
+//             resizedDataUrl = canvas.toDataURL('image/jpeg');
+//             imageTag.src = resizedDataUrl;
 
-            imageTag.classList.remove('hide');
-            label.querySelector('svg').style.display = 'none';
-            label.querySelectorAll('span').forEach((span) => {
-                span.style.display = 'none';
-            })
-            document.querySelector('.error-div').classList.add('hide');
-            document.querySelector('.create-error-msg').classList.remove('active');
-            document.querySelector('.create-error-msg').innerText = "";
-        };
-    };
+//             imageTag.classList.remove('hide');
+//             label.querySelector('svg').style.display = 'none';
+//             label.querySelectorAll('span').forEach((span) => {
+//                 span.style.display = 'none';
+//             })
+//             document.querySelector('.error-div').classList.add('hide');
+//             document.querySelector('.create-error-msg').classList.remove('active');
+//             document.querySelector('.create-error-msg').innerText = "";
+//         };
+//     };
 
-    reader.readAsDataURL(file);
-}
+//     reader.readAsDataURL(file);
+// }
 
 
 // Old method to only accept images of dimensions 330 x 330
 
-// function verifyEventImage(event, input) {
-//     if (input.files.length > 0) {
-//         let label = input.closest('label');
-//         let width = 330;
-//         let height = 330;
-//         const img = document.createElement('img');
-//         const selectedImage = input.files[0];
-//         const objectURL = URL.createObjectURL(selectedImage);
-//         let imageTag = label.querySelector('.event-img');
-//         img.onload = function handleLoad() {
-//             // console.log(`Width: ${img.width}, Height: ${img.height}`);
+function verifyEventImage(event, input) {
+    if (input.files.length > 0) {
+        let label = input.closest('label');
+        let width = 330;
+        let height = 330;
+        const img = document.createElement('img');
+        const selectedImage = input.files[0];
+        const objectURL = URL.createObjectURL(selectedImage);
+        let imageTag = label.querySelector('.event-img');
+        img.onload = function handleLoad() {
+            // console.log(`Width: ${img.width}, Height: ${img.height}`);
     
-//             if (img.width == width && img.height == height) {
-//                 imageTag.src = objectURL;
-//                 imageTag.classList.remove('hide');
-//                 label.querySelector('svg').style.display = 'none';
-//                 label.querySelectorAll('span').forEach((span) => {
-//                     span.style.display = 'none';
-//                 })
-//                 document.querySelector('.error-div').classList.add('hide');
-//                 document.querySelector('.create-error-msg').classList.remove('active');
-//                 document.querySelector('.create-error-msg').innerText = "";
-//             }
-//             else {
-//                 URL.revokeObjectURL(objectURL);
-//                 imageTag.classList.add('hide');
-//                 label.querySelector('svg').style.display = 'block';
-//                 label.querySelectorAll('span').forEach((span) => {
-//                     span.style.display = 'block';
-//                 })
-//                 document.querySelector('.error-div').classList.remove('hide');
-//                 document.querySelector('.create-error-msg').classList.add('active');
-//                 document.querySelector('.create-error-msg').innerText = "Image does not match supported dimensions: 330x330 px";
-//                 input.value = null;
-//             }
-//         };
+            if (img.width == width && img.height == height) {
+                imageTag.src = objectURL;
+                imageTag.classList.remove('hide');
+                label.querySelector('svg').style.display = 'none';
+                label.querySelectorAll('span').forEach((span) => {
+                    span.style.display = 'none';
+                })
+                document.querySelector('.error-div').classList.add('hide');
+                document.querySelector('.create-error-msg').classList.remove('active');
+                document.querySelector('.create-error-msg').innerText = "";
+            }
+            else {
+                URL.revokeObjectURL(objectURL);
+                imageTag.classList.add('hide');
+                label.querySelector('svg').style.display = 'block';
+                label.querySelectorAll('span').forEach((span) => {
+                    span.style.display = 'block';
+                })
+                document.querySelector('.error-div').classList.remove('hide');
+                document.querySelector('.create-error-msg').classList.add('active');
+                document.querySelector('.create-error-msg').innerText = "Image does not match supported dimensions: 330x330 px";
+                input.value = null;
+            }
+        };
   
-//         img.src = objectURL;
-//     }
-// }
+        img.src = objectURL;
+    }
+}
 
 
 function dataURItoBlob(dataURI, type, filename) {
@@ -178,21 +178,21 @@ async function createEventForm(event) {
         errorMsg.classList.add('active');
         return false;
     }
-    // else if (imageInput.files.length == 0) {
-    //     errorDiv.classList.remove('hide');
-    //     errorMsg.innerText = 'Image with supported dimensions is required';
-    //     errorMsg.classList.add('active');
-    //     return false;
-    // }
-    else if (resizedDataUrl == undefined) {
+    else if (imageInput.files.length == 0) {
         errorDiv.classList.remove('hide');
-        errorMsg.innerText = 'Image is required';
+        errorMsg.innerText = 'Image with supported dimensions is required';
         errorMsg.classList.add('active');
         return false;
     }
+    // else if (resizedDataUrl == undefined) {
+    //     errorDiv.classList.remove('hide');
+    //     errorMsg.innerText = 'Image is required';
+    //     errorMsg.classList.add('active');
+    //     return false;
+    // }
     else {
-        formData.delete('image');
-        formData.append('image', dataURItoBlob(resizedDataUrl, 'image/jpeg', 'image.jpg'), 'image.jpg');
+        // formData.delete('image');
+        // formData.append('image', dataURItoBlob(resizedDataUrl, 'image/jpeg', 'image.jpg'), 'image.jpg');
         try {
             errorDiv.classList.add('hide');
             errorMsg.innerText = '';
