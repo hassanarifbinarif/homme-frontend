fbda8c9c0cc2b96f58fba295f40153aa40dcfad2
Ammar Munir <munirammar0@gmail.com>
2024-08-29 12:36:24 +0500

    Add view Source modal on Create Source page and fix the modal for supporting in the both views.

--- static/Assets/modals/js/viewSourceModal.js
+++ static/Assets/modals/js/viewSourceModal.js
@@ -6,12 +6,12 @@ function openViewSourceModal(element) {
     const embeddedString = element.querySelector('td:nth-child(10) .table-text-overflow').textContent.trim();
     const description = element.querySelector('td:nth-child(11) .table-text-overflow').textContent.trim();
 
-    document.querySelector('input[name="source_channel"]').value = channelName;
-    document.querySelector('input[name="source_owner"]').value = ownerName;
-    document.querySelector('input[name="source_type"]').value = typeName;
+    document.querySelector('input[name="channelName"]').value = channelName;
+    document.querySelector('input[name="ownerName"]').value = ownerName;
+    document.querySelector('input[name="typeName"]').value = typeName;
     document.querySelector('input[name="sourceName"]').value = sourceName;
-    document.querySelector('input[name="embedded_string"]').value = embeddedString;
-    document.querySelector('textarea[name="description"]').value = description;
+    document.querySelector('input[name="embeddedString"]').value = embeddedString;
+    document.querySelector('textarea[name="desc"]').value = description;
 
     document.querySelector('.viewSource').click();
 }
